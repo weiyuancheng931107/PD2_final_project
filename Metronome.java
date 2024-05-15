51ca5fe525331fa8c41a799313a86b2b54a595cd
weiyuancheng931107 <F74121050@gs.ncku.edu.tw>
2024-05-15 22:52:20 +0800

    Update Metronome.java

--- Metronome.java
+++ Metronome.java
@@ -3,28 +3,33 @@ package com.musicgenreclassifier;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+
+import javax.management.relation.RoleList;
 import javax.sound.midi.*;
 
 class Metronome {
     private Sequence sequence;
     private Track track;
-    private int bpm;
-    private static int program;
-    private ArrayList<Integer> notename;
-    private int pitch;
-    private int velocity;
-    private int beat;
-    private int channel;
-    public static int tick =0;
+    int bpm;
+    static int program;
+    ArrayList<Integer> notename;
+    int pitch;
+    int velocity;
+    ArrayList<Integer> beat;
+    int channel;
+    int tick =0;
     private Sequencer sequencer;
-    public Metronome(int bpm, int program, ArrayList<Integer> notename, int pitch, int velocity, double beat,int channel) throws InvalidMidiDataException, MidiUnavailableException {
+    public Metronome(int bpm, int program, ArrayList<Integer> notename, int pitch, int velocity, ArrayList<Integer> beat,int channel) throws InvalidMidiDataException, MidiUnavailableException {
         this.bpm = bpm;
         Metronome.program = program;
         this.notename = notename;
         this.pitch = pitch;
         this.velocity = velocity;
         this.channel = channel;
-        this.beat = (int) (120 / beat);
+        this.beat = new ArrayList<>();
+        for(int i =0;i<beat.size();i++){
+            this.beat.add((int)(120/beat.get(i)));
+        }
         this.sequence = new Sequence(Sequence.PPQ, bpm);
         this.track = (this.sequence).createTrack();
         sequencer = MidiSystem.getSequencer(); // 获取 sequencer
@@ -51,30 +56,34 @@ class Metronome {
         return velocity;
     }
 
-    public int getBeat() {
+    public ArrayList<Integer> getBeat() {
         return this.beat;
     }
-
+    public int getTick() {
+        return tick;
+    }
     public void rhythmchord() throws InvalidMidiDataException {
         // 添加MIDI事件：音符ON
-        for(int i = 0;i<notename.size();i++){
-            if(notename.get(i) == -1){
-                break;
+        for(int j =0;j<beat.size();j++){
+            for(int i = 0;i<notename.size();i++){
+                if(notename.get(i) == -1){
+                    break;
+                }
+                ShortMessage noteOn = new ShortMessage();
+                noteOn.setMessage(ShortMessage.NOTE_ON, channel, notename.get(i), velocity);
+                track.add(new MidiEvent(noteOn, tick));
             }
-            ShortMessage noteOn = new ShortMessage();
-            noteOn.setMessage(ShortMessage.NOTE_ON, channel, notename.get(i), velocity);
-            track.add(new MidiEvent(noteOn, tick));
+            // 添加MIDI事件：音符OFF
+            for(int i =0;i<notename.size();i++){
+                if(notename.get(i) == -1){
+                    continue;
+                }
+                ShortMessage noteOff = new ShortMessage();
+                noteOff.setMessage(ShortMessage.NOTE_OFF, channel, notename.get(i), velocity);
+                track.add(new MidiEvent(noteOff, tick + this.beat.get(j))); 
+                tick += this.beat.get(j);
         }
-        // 添加MIDI事件：音符OFF
-        for(int i =0;i<notename.size();i++){
-            if(notename.get(i) == -1){
-                continue;
-            }
-            ShortMessage noteOff = new ShortMessage();
-            noteOff.setMessage(ShortMessage.NOTE_OFF, channel, notename.get(i), velocity);
-            track.add(new MidiEvent(noteOff, tick + this.beat)); 
-        }
-        tick += this.beat;
+    }
     }
     public void rhythmonenote() throws InvalidMidiDataException {
         // 添加MIDI事件：音符ON
@@ -88,8 +97,8 @@ class Metronome {
         // 添加MIDI事件：音符OFF
             ShortMessage noteOff = new ShortMessage();
             noteOff.setMessage(ShortMessage.NOTE_OFF, channel, notename.get(i), velocity);
-            track.add(new MidiEvent(noteOff, tick + this.beat)); 
-            tick += this.beat;
+            track.add(new MidiEvent(noteOff, tick + this.beat.get(i))); 
+            tick += this.beat.get(i);
         }
     }
     public static MidiEvent createProgramChangeMessage() throws InvalidMidiDataException {
