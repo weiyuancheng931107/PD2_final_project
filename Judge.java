06cea54a29f3c874b2d90daa34db6d6f80e76cfa
weiyuancheng931107 <F74121050@gs.ncku.edu.tw>
2024-05-20 13:45:13 +0800

    Update Judge.java

--- Judge.java
+++ Judge.java
@@ -1,8 +1,14 @@
 package com.musicgenreclassifier;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
+import java.util.Random;
+
+import javax.sound.midi.InvalidMidiDataException;
+import javax.sound.midi.MidiUnavailableException;
 
 public class Judge {
     Chord C;
@@ -18,11 +24,11 @@ public class Judge {
     Chord Asharp;
     Chord B;
     ArrayList<Integer> notename;
-    ArrayList<Integer> beat;
+    ArrayList<Double> beat;
     int meter;
     ArrayList<ArrayList<Integer>> chordList;
 
-    public Judge(ArrayList<Integer> notename, ArrayList<Integer> beat, int meter) {
+    public Judge(ArrayList<Integer> notename, ArrayList<Double> beat, int meter) {
         this.C = new Chord(0, -1);
         this.Csharp = new Chord(1, -1);
         this.D = new Chord(2, -1);
@@ -75,10 +81,15 @@ public class Judge {
         }
 
         ArrayList<ArrayList<Integer>> originalChordList = new ArrayList<>(chordList);
-
+        
+        // ArrayList<ArrayList<Integer>> currentChordList = new ArrayList<>(originalChordList);
+        ArrayList<ArrayList<Integer>> currentChordList = new ArrayList<>(originalChordList);
+        // System.out.println(originalChordList);
+        // System.out.println("-======================================");
+        Iterator<ArrayList<Integer>> iterator = currentChordList.iterator();
         for (int i = 0; i < beat.size(); i++) {
-            ArrayList<ArrayList<Integer>> currentChordList = new ArrayList<>(originalChordList);
-            Iterator<ArrayList<Integer>> iterator = currentChordList.iterator();
+            // ArrayList<ArrayList<Integer>> currentChordList = new ArrayList<>(originalChordList);
+            // Iterator<ArrayList<Integer>> iterator = currentChordList.iterator();
             while (iterator.hasNext()) {
                 ArrayList<Integer> currentChord = iterator.next();
                 if (!currentChord.contains(notename.get(i))) {
@@ -86,15 +97,94 @@ public class Judge {
                 }
             }
             if (parameter <= 1) {
+                // System.out.println(currentChordList);
                 judgements.put(j + 1, currentChordList);
             }
             parameter += (double) (1.0 / beat.get(i));
-            while (parameter >= 1) {
+            while (Math.abs(parameter - 1) <= 0.01 || parameter >= 1) {
+                System.out.println("asdasd, " + i);
+                if (parameter > 1) {
+                    currentChordList = new ArrayList<>(originalChordList);
+                    iterator = currentChordList.iterator();
+                    while (iterator.hasNext()) {
+                        ArrayList<Integer> currentChord = iterator.next();
+                        if (!currentChord.contains(notename.get(i))) {
+                            iterator.remove();
+                        }
+                    }
+                } else {
+                    currentChordList = new ArrayList<>(originalChordList);
+                }
                 parameter -= 1;
                 j++;
+                // judgements.put(j, currentChordList);
+                
             }
+            iterator = currentChordList.iterator();
         }
 
         return judgements;
     }
+
+    public HashMap<Integer, ArrayList<Integer>> getRepeat(HashMap<Integer, ArrayList<ArrayList<Integer>>> judgements) {
+        HashMap<Integer, ArrayList<Integer>> repeat = new HashMap<>();
+        System.out.println(judgements);
+        Random random = new Random();
+
+        List<Integer> keys = new ArrayList<>(judgements.keySet());
+        for (int i = 0; i < keys.size() - 1; i++) {
+            int key1 = keys.get(i);
+            ArrayList<ArrayList<Integer>> judgeList1 = judgements.get(key1);
+            boolean found = false;
+
+            for (int j = i + 1; j < keys.size(); j++) {
+                int key2 = keys.get(j);
+                ArrayList<ArrayList<Integer>> judgeList2 = judgements.get(key2);
+
+                for (ArrayList<Integer> judge1 : judgeList1) {
+                    if (found) break;
+                    for (ArrayList<Integer> judge2 : judgeList2) {
+                        if (judge1.equals(judge2)) {
+                            repeat.put(key1, judge1);
+                            repeat.put(key2, judge1);
+                            found = true;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            if (!found) {
+                int randomIndex1 = random.nextInt(judgeList1.size());
+                repeat.put(key1, judgeList1.get(randomIndex1));
+
+                for (int j = 0; j < keys.size(); j++) {
+                    if (i == j) continue;
+                    int key2 = keys.get(j);
+                    ArrayList<ArrayList<Integer>> judgeList2 = judgements.get(key2);
+                    int randomIndex2 = random.nextInt(judgeList2.size());
+                    repeat.put(key2, judgeList2.get(randomIndex2));
+                }
+            }
+        }
+
+        System.out.println(repeat);
+        return repeat;
+    }
+
+    public void music() throws InvalidMidiDataException, MidiUnavailableException, IOException{
+        HashMap<Integer, ArrayList<ArrayList<Integer>>> a = new HashMap<>();
+        a = judgement();
+        ArrayList<Double> b = new ArrayList<>();
+        b.add(1.0);
+        b.add(1.0);
+        b.add(1.0);
+        b.add(1.0);
+        Metronome s = new Metronome(120, 29, a.get(1).get(3), 4, 100,b, 0);
+
+        // s.rhythmchord();
+        s.rhythmonenote();
+        s.writeToFile("output");
+    }
+
 }
