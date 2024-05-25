dbb008dff6104422b83774da67d80eceeafefc72
chang21700000228106 <80868081+chang21700000228106@users.noreply.github.com>
2024-05-25 15:05:10 +0800

    Update Metronome.java
     notename從兩層的arraylist改成一層

--- Metronome.java
+++ Metronome.java
@@ -1,16 +1,16 @@
-package com.musicgenreclassifier;
+
 
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import javax.sound.midi.*;
 
-class Metronome {
+class Metronome1 {
     private Sequence sequence;
     private Track track;
     int bpm;
     static int program;
-    ArrayList<ArrayList<Integer>> notename;
+    ArrayList<Integer> notename;
     int pitch;
     int velocity;
     ArrayList<Integer> beat;
@@ -18,17 +18,12 @@ class Metronome {
     int tick = 0;
     private Sequencer sequencer;
 
-    public Metronome(int bpm, int program, ArrayList<ArrayList<Integer>> notename, int pitch, int velocity, ArrayList<Double> beat, int channel) throws InvalidMidiDataException, MidiUnavailableException {
+    public Metronome1(int bpm, int program, ArrayList<Integer> notename, int pitch, int velocity, ArrayList<Double> beat, int channel) throws InvalidMidiDataException, MidiUnavailableException {
         this.bpm = bpm;
         Metronome.program = program;
         this.notename = new ArrayList<>();
         for (int i = 0; i < notename.size(); i++) {
-            ArrayList<Integer> temp = new ArrayList<>(); // 每次迭代都创建一个新的temp列表
-            for (int j = 0; j < notename.get(i).size(); j++) {
-                Note note = new Note(notename.get(i).get(j) % 12, pitch);
-                temp.add(note.getNote());
-            }
-            this.notename.add(temp); // 将每个新创建的temp列表添加到notename中
+            this.notename = notename;
         }
         this.pitch = pitch;
         this.velocity = velocity;
@@ -54,7 +49,7 @@ class Metronome {
         return program;
     }
 
-    public ArrayList<ArrayList<Integer>> getNotename() {
+    public ArrayList<Integer> getNotename() {
         return notename;
     }
 
@@ -77,25 +72,22 @@ class Metronome {
     public void rhythmchord() throws InvalidMidiDataException {
         // Add MIDI events: note ON
         for (int j = 0; j < beat.size(); j++) {
-                for(int k = 0;k<notename.get(j).size();k++){
-                    if (notename.get(j).get(k) == -1) {
-                        break;
+                    if (notename.get(j) == -1) {
+                        tick+=this.beat.get(j);
+                        continue;
                     }
                     ShortMessage noteOn = new ShortMessage();
-                    noteOn.setMessage(ShortMessage.NOTE_ON, channel, notename.get(j).get(k), velocity);
+                    noteOn.setMessage(ShortMessage.NOTE_ON, channel, notename.get(j), velocity);
                     track.add(new MidiEvent(noteOn, tick));
-                }
+                
                 // Add MIDI events: note OFF
 
-                for(int k = 0;k<notename.get(j).size();k++){
-                    if (notename.get(j).get(k) == -1) {
-                        break;
-                    }
-                    ShortMessage noteOff = new ShortMessage();
-                    noteOff.setMessage(ShortMessage.NOTE_OFF, channel, notename.get(j).get(k), velocity);
-                    track.add(new MidiEvent(noteOff, tick + this.beat.get(j)));
+                if (notename.get(j) == -1) {
+                    continue;
                 }
-            
+                ShortMessage noteOff = new ShortMessage();
+                noteOff.setMessage(ShortMessage.NOTE_OFF, channel, notename.get(j), velocity);
+                track.add(new MidiEvent(noteOff, tick + this.beat.get(j)));
                 tick+=this.beat.get(j);
             }
         }
