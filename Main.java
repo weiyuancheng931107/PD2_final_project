b3f15098c4a007800f4c9cf2d44aebf0c6b889c0
chang21700000228106 <80868081+chang21700000228106@users.noreply.github.com>
2024-06-09 18:12:17 +0800

    Update Main.java
    changing some input arguments

--- Main.java
+++ Main.java
@@ -2,13 +2,16 @@ package com.musicgenreclassifier;
 
 import javax.sound.midi.InvalidMidiDataException;
 import javax.sound.midi.MidiUnavailableException;
+
+import com.musicgenreclassifier.MidiMerger;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
-    public static void main(String[] args) throws InvalidMidiDataException, MidiUnavailableException, IOException {
+    public static void main(String[] args) throws InvalidMidiDataException, MidiUnavailableException, IOException , InterruptedException{
         int bpm = 120;
         int velocity = 100;
 
@@ -47,13 +50,22 @@ public class Main {
 
         Judge judge = new Judge(noteSequence, noteDurations, velocity);
         Map<Integer, ArrayList<Integer>> chordMap = judge.judgement();
-        MidiGenerator midiGenerator = new MidiGenerator(bpm, 54, noteSequence, bpm, velocity, noteDurations, 0);
+        MidiGenerator midiGenerator = new MidiGenerator(bpm, 56, noteSequence, bpm, velocity, noteDurations, 0);
         midiGenerator.playRhythm();
-        midiGenerator.saveToFile("output");
+        midiGenerator.saveToFile("output.mid");
         Jazz rock= new Jazz(bpm, velocity, chordMap);
+        //Rock rock= new Rock(bpm, velocity, chordMap);
         rock.Piano();
         rock.Drums();
         rock.Bass();
-        // rock.Guitar();
+
+        //rock.Guitar();
+        MidiMerger midimerge = new MidiMerger();
+        //MidiMergerTry midimergetry = new MidiMergerTry();
+        //String[] midiFiles = {"piano.mid","guitar.mid", "bass.mid","output.mid","drums.mid"};
+        String[] midiFiles = {"drums.mid","piano.mid","bass.mid","output.mid"};
+        String outputFile = "merged.mid";
+        midimerge.mergeMidiFiles(midiFiles,outputFile);
+        //midimerge.convertAndMergeMidiFiles(midiFiles,outputFile);
     }
 }
