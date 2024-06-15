b42a90982e3aeccb633513ad2362c7bbd97d60c4
chang21700000228106 <80868081+chang21700000228106@users.noreply.github.com>
2024-06-16 05:10:28 +0800

    Update MusicApp.java

--- musicgenreclassifier/src/main/java/com/musicgenreclassifier/MusicApp.java
+++ musicgenreclassifier/src/main/java/com/musicgenreclassifier/MusicApp.java
@@ -42,7 +42,7 @@ public class MusicApp extends JPanel {
 
         // Create and configure notation panel
         MusicNotation notationPanel = new MusicNotation();
-        notationPanel.setBounds(0, 0, 3000, 300);
+        notationPanel.setBounds(0, 0, 1700, 700);
         notationPanel.setOpaque(false);
 
         // Add panels to layered pane
@@ -205,6 +205,7 @@ public class MusicApp extends JPanel {
 
         MusicApp musicApp = new MusicApp();
         frame.add(musicApp);
+
 
         frame.setVisible(true);
     }
